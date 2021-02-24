269bd84cb6732099e02627e2ecd505cf81a9ed76
Lasha Sherozia <repeop0@gmail.com>
2021-02-25 02:19:54 +0400

    Added second page parser, so it follows all models of given make.

--- nrp/nrp/spiders/cars.py
+++ nrp/nrp/spiders/cars.py
@@ -6,7 +6,7 @@ class CarsSpider(scrapy.Spider):
     name = 'car'
 
     def start_requests(self):
-        url = "https://nrp-performance.file-service.com/tuning-specs/lamborghini-tractors"
+        url = "https://nrp-performance.file-service.com/tuning-specs"
         yield scrapy.Request(url, callback=self.parse_first_page)
 
     def parse_first_page(self, response: HtmlResponse):
@@ -17,5 +17,11 @@ class CarsSpider(scrapy.Spider):
             car = response.urljoin('/tuning-specs/' + car)
             yield scrapy.Request(car, callback=self.parse_second_page)
 
-    def parse_second_page(self, response):
-        pass
+    def parse_second_page(self, response: HtmlResponse):
+        # last link follows to 'overview' page, so we don't need it
+        models = response.xpath("//a[@class='btn btn--ghost border']/"
+                                "@href").getall()[:-1]
+        yield from response.follow_all(models, callback=self.parse_third_page)
+
+    def parse_third_page(self, response):
+        yield {'model': response.url}
